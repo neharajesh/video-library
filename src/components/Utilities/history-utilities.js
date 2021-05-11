73f6b90a7853ade82cfda2ad00d0f49208c824ef
neharajesh <leehanseok3@gmail.com>
2021-05-12 01:57:38 +0530

    update - removed comments

--- src/components/Utilities/history-utilities.js
+++ src/components/Utilities/history-utilities.js
@@ -1,24 +1,16 @@
 export const addToHistory = (videoId, videoHistoryList) => {
-    //check if video id is there in videoHistoryList
     const existingVideo = videoHistoryList.find(video => video.id === videoId);
-    //if it exists, do not add, just return the existing list
     if(!existingVideo) {
         return [...videoHistoryList, existingVideo];
     }
-    //if it does not exist, add to the list
-    else {
-        return videoHistoryList
-    }
+    return videoHistoryList
 }
 
 export const removeFromHistory = (videoId, videoHistoryList) => {
-    //check if video id exists in videoHistoryList
     const existingVideo = videoHistoryList.find(video => video.id === videoId)
-    //if it exists, remove it
     if(!existingVideo) {
         return videoHistoryList
     }
-    //if it does not, return videoHistoryList
     const updatedList = videoHistoryList.filter(video => video.id === videoId)
     return updatedList
 }
