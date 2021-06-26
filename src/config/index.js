9938ce781ef4e9c0dd7380034d3e8351715b71ed
neharajesh <leehanseok3@gmail.com>
2021-06-26 19:27:18 +0530

    FIX: Minor Bug

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-const ROOT_URL = "http://localhost:5000"; //add localhost or repl url here
+//const ROOT_URL = "http://localhost:5000"; //add localhost or repl url here
 
-//const ROOT_URL = "https://video-library-v1-backend.herokuapp.com" //herokuapp
+const ROOT_URL = "https://video-library-v1-backend.herokuapp.com" //herokuapp
 
 export default ROOT_URL
